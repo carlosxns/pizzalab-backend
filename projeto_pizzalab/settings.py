5124a2331238c2d2cfd1fd1198015551b09de437
Carlos Xavier Nicodemus dos Santos <carlos20250003581@alu.uern.br>
2026-05-13 18:17:06 -0300

    fix: esconde SECRET_KEY e protege ambiente

--- projeto_pizzalab/settings.py
+++ projeto_pizzalab/settings.py
@@ -13,19 +13,20 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cn1ild9h&lnuaiy49hxt$#hc6e=ndr0*h*cmu$cf)oi1!o8ga)'
+# Puxa a chave do .env (local) ou do Render (produção)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Fica False no Render, mas True no seu PC se você configurar no .env
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
